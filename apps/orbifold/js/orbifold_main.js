6117c11237787b765e8651f227a3cb04201ce4ca
Vladimir Bulatov <vladimir@bulatov.org>
2025-02-13 13:49:20 -0800

    testing apps/orbifold/

--- apps/orbifold/js/orbifold_main.js
+++ apps/orbifold/js/orbifold_main.js
@@ -1,147 +1,153 @@
- const FRAG_FOLDER = 'js/frag/';
+const FRAG_FOLDER = 'js/frag/';
 
- import {
-     Textures,
-     DefaultDomainBuilder as DomainBuilder,
-     PatternTextures,
-     GroupRenderer,
-     WallPaperGroup_General,
-     SymmetryUIController,
- }
- from './modules.js';
+import {
+    Textures,
+    DefaultDomainBuilder as DomainBuilder,
+    PatternTextures,
+    GroupRenderer,
+    WallPaperGroup_General,
+    SymmetryUIController,
+}
+from './modules.js';
 
+// if "active", overlay yellow and thicken.
+const STYLES = {
+    "activeColor": {
+        color: "#FFFF00",
+        width: 12
+    },
+    "handle": {
+        color: "#0000FF",
+        width: 4
+    },
+    "cap": {
+        color: "#8C0072",
+        width: 4
+    },
+    "conePt": {
+        color: "#AA00E1",
+        width: 2
+    }, //should'nt come up yet
+    "conePair": {
+        color: "#0000E1",
+        width: 4
+    },
+    "tube": {
+        color: "#EBAD00",
+        width: 4
+    },
+    "mirror": {
+        color: "#DB00FF",
+        width: 4
+    },
+    "mirrorRedundant": {
+        color: "#DB0000",
+        width: 4
+    },
+    "cornerPair": {
+        color: "#DB0000",
+        width: 4
+    },
+    "fold": {
+        color: "#DB0000",
+        width: 4
+    },
+    "band": {
+        color: "#EBAD00",
+        width: 4
+    },
+    "slice": {
+        color: "#EBAD00",
+        width: 4
+    },
+    "cuttingEdge": {
+        color: "#92C4DD",
+        width: 2
+    },
+    "edge": {
+        color: "#92C4DD",
+        width: 4
+    },
 
- // if "active", overlay yellow and thicken.
- const STYLES = {
-     "activeColor": {
-         color: "#FFFF00",
-         width: 12
-     },
-     "handle": {
-         color: "#0000FF",
-         width: 4
-     },
-     "cap": {
-         color: "#8C0072",
-         width: 4
-     },
-     "conePt": {
-         color: "#AA00E1",
-         width: 2
-     }, //should'nt come up yet
-     "conePair": {
-         color: "#0000E1",
-         width: 4
-     },
-     "tube": {
-         color: "#EBAD00",
-         width: 4
-     },
-     "mirror": {
-         color: "#DB00FF",
-         width: 4
-     },
-     "mirrorRedundant": {
-         color: "#DB0000",
-         width: 4
-     },
-     "cornerPair": {
-         color: "#DB0000",
-         width: 4
-     },
-     "fold": {
-         color: "#DB0000",
-         width: 4
-     },
-     "band": {
-         color: "#EBAD00",
-         width: 4
-     },
-     "slice": {
-         color: "#EBAD00",
-         width: 4
-     },
-     "cuttingEdge": {
-         color: "#92C4DD",
-         width: 2
-     },
-     "edge": {
-         color: "#92C4DD",
-         width: 4
-     },
+    "domainInterior": {
+        color: ""
+    }, //alphaGray
+}
 
-     "domainInterior": {
-         color: ""
-     }, //alphaGray
- }
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
 
- //////////////////////////////////////////////
- //////////////////////////////////////////////
- //////////////////////////////////////////////
+// GLOBALS
 
- // GLOBALS
+const PRE_FOLDER = 'presets/';
+const JSONpresets = [
+    {
+        name: '3333',
+        path: PRE_FOLDER + '3333.json'
+    }, {
+        name: '32x',
+        path: PRE_FOLDER + '32x.json'
+    }, {
+        name: 'xx2',
+        path: PRE_FOLDER + 'xx2.json'
+    }, {
+        name: 'xo',
+        path: PRE_FOLDER + 'xo.json'
+    }, {
+        name: '325',
+        path: PRE_FOLDER + '325.json'
+    }, {
+        name: '326',
+        path: PRE_FOLDER + '326.json'
+    }, {
+        name: '327',
+        path: PRE_FOLDER + '327.json'
+    }, {
+        name: '327 a',
+        path: PRE_FOLDER + '327_a.json'
+    },
+];
 
- const PRE_FOLDER = 'presets/';
- const JSONpresets = [{
-         name: '32x',
-         path: PRE_FOLDER + '32x.json'
-     }, {
-         name: 'xx2',
-         path: PRE_FOLDER + 'xx2.json'
-     }, {
-         name: 'xo',
-         path: PRE_FOLDER + 'xo.json'
-     }, {
-         name: '325',
-         path: PRE_FOLDER + '325.json'
-     }, {
-         name: '326',
-         path: PRE_FOLDER + '326.json'
-     }, {
-         name: '327',
-         path: PRE_FOLDER + '327.json'
-     },
- ];
+var grouphandler = new WallPaperGroup_General({
+    symmetryUI:
+    new SymmetryUIController({
+        domainShowingQ: true,
+        overlayCanvas: document.getElementById('overlay'),
+        styles: STYLES
+    })
+})
 
- var grouphandler = new WallPaperGroup_General({
-     symmetryUI:
-     new SymmetryUIController({
-         domainShowingQ: true,
-         overlayCanvas: document.getElementById('overlay'),
-         styles: STYLES
-     })
- })
+const maxgenCount = 20;
 
- const maxgenCount = 20;
+let render = new GroupRenderer({
+    glCanvas: document.getElementById('glCanvas'),
+    overlayCanvas: document.getElementById('overlay'),
+    groupMaker: grouphandler,
+    patternMaker: new PatternTextures({
+        textures: [Textures.t2, Textures.t1]
+    }),
+    domainBuilder: new DomainBuilder({
+        MAX_GEN_COUNT: maxgenCount,
+        MAX_TOTAL_REF_COUNT: 30,
+        USE_PACKING: true
+    }),
+    guiOrder: ["group", "domain"],
+    fragShader: [
+        FRAG_FOLDER + "fsMain.frag",
+        FRAG_FOLDER + "inversive.frag",
+        FRAG_FOLDER + "complex.frag",
+        FRAG_FOLDER + "patternTextures.frag",
+        FRAG_FOLDER + "generalGroupMain_v2.frag"
+    ],
+    vertShader: [
+        FRAG_FOLDER + "vertexShader.frag",
+    ],
+    JSONpresets: JSONpresets,
+    JSONpreset: JSONpresets[0].name,
+    useParamGui: false,
+});
 
- let render = new GroupRenderer({
-     glCanvas:      document.getElementById('glCanvas'),
-     overlayCanvas: document.getElementById('overlay'),
-     groupMaker:    grouphandler,
-     patternMaker:  new PatternTextures({
-                            textures: [Textures.t2, Textures.t1]
-                        }),
-     domainBuilder: new DomainBuilder({
-                            MAX_GEN_COUNT: maxgenCount,
-                            MAX_TOTAL_REF_COUNT: 30,
-                            USE_PACKING: true
-                        }),
-     guiOrder:      ["group", "domain"],
-     fragShader:    [
-                        FRAG_FOLDER + "fsMain.frag",
-                        FRAG_FOLDER + "inversive.frag",
-                        FRAG_FOLDER + "complex.frag",
-                        FRAG_FOLDER + "patternTextures.frag",
-                        FRAG_FOLDER + "generalGroupMain_v2.frag"
-                    ],
-     vertShader:    [
-                        FRAG_FOLDER + "vertexShader.frag",     
-                    ],
-     JSONpresets:   JSONpresets,
-     JSONpreset:    JSONpresets[0].name,
-     useParamGui:   false,
- });
+render.init();
 
- render.init();
-
- //  symmetryUI.init();
+//  symmetryUI.init();
