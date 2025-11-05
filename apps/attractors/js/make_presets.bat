c838f8f25f2a669a63ca052c1de0801219cfc7d1
Vladimir Bulatov <vladimir@bulatov.org>
2025-11-05 15:49:13 -0800

    added examples of iterated attractors without symmetry

--- apps/attractors/js/make_presets.bat
+++ apps/attractors/js/make_presets.bat
@@ -1,6 +1,8 @@
-call make_preset ..\presets\wp\*.json presets_wp.js
-call make_preset ..\presets\frieze\*.json presets_frieze.js
-call make_preset ..\presets\spherical\*.json presets_spherical.js
-call make_preset ..\presets\klm\*.json presets_klm.js
+rem call make_preset ..\presets\wp\*.json presets_wp.js
+rem call make_preset ..\presets\frieze\*.json presets_frieze.js
+rem call make_preset ..\presets\spherical\*.json presets_spherical.js
+rem call make_preset ..\presets\klm\*.json presets_klm.js
+rem call make_preset ..\presets\klm\*.json presets_klm.js
+call make_preset ..\presets\nosym\*.json presets_nosym.js
 pause
 
