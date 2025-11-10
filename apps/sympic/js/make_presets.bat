67473e8a118dd46cea8062fd3fd72050d622f8b3
Vladimir Bulatov <vladimir@bulatov.org>
2025-11-10 09:22:21 -0800

    support for SymPix spherical and frieze symmetries

--- apps/sympic/js/make_presets.bat
+++ apps/sympic/js/make_presets.bat
@@ -3,5 +3,8 @@ rem call make_preset ..\presets\wp\*.json presets_wp.js
 rem call make_preset ..\presets\klm\*.json presets_klm.js
 call make_preset ..\presets\klmn\*.json presets_klmn.js
 call make_preset ..\presets\5splanes\*.json presets_5splanes.js
+call make_preset ..\presets\frieze\*.json presets_frieze.js
+call make_preset ..\presets\spherical\*.json presets_spherical.js
+
 pause
 
---
+++ apps/sympic/js/sympix_frieze.js
@@ -0,0 +1,39 @@
+
+import { 
+    Group_Frieze, 
+    InversiveNavigator,
+    VisualizationManager,
+    VisualizationImage,
+    VisualizationOverlay,
+    SymRenderer,
+    makeSamplesArray,
+    PatternImageCreator,
+} from "./modules.js";
+
+import {
+    presets
+} from './presets_frieze.js';
+ 
+const visManager = VisualizationManager({
+      visLayers: [
+        {
+            name: 'image',
+            visLayer: VisualizationImage(),
+        },
+        {
+            name: 'overlay',
+            visLayer: VisualizationOverlay(),
+        }
+      ]
+});
+
+const app = SymRenderer({
+      simCreator:       PatternImageCreator,
+      visualization:    visManager, 
+      groupMaker:       new Group_Frieze(), // maker of the groups
+      navigator:        new InversiveNavigator(),
+      samples:          makeSamplesArray(presets, 'presets/frieze/'),
+});
+
+app.run();
+
