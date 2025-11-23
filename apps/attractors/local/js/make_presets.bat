d21c7d8731e2f3836b337b0f1dc5fc59c831b2ba
Vladimir Bulatov <vladimir@bulatov.org>
2025-11-22 16:28:41 -0800

    adding local examples

---
+++ apps/attractors/local/js/make_presets.bat
@@ -0,0 +1,12 @@
+rem 
+call make_preset ..\presets\spherical\*.json presets_spherical.js
+rem 
+call make_preset ..\presets\klm\*.json presets_klm.js
+call make_preset ..\presets\frieze\*.json presets_frieze.js
+rem call make_preset ..\presets\klmn\*.json presets_klmn.js
+rem 
+call make_preset ..\presets\wp\*.json presets_wp.js
+call make_preset ..\presets\nosym\*.json presets_nosym.js
+rem call make_preset ..\presets\5splanes\*.json presets_5splanes.js
+pause
+
