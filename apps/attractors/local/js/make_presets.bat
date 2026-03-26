a3e37236833f7fb01bf82c2322d27745603d2bb1
Vladimir Bulatov <vladimir@bulatov.org>
2026-03-26 13:01:17 -0700

    clean up

--- apps/attractors/local/js/make_presets.bat
+++ apps/attractors/local/js/make_presets.bat
@@ -3,7 +3,8 @@ call make_preset ..\presets\spherical\*.json presets_spherical.js
 rem 
 call make_preset ..\presets\klm\*.json presets_klm.js
 call make_preset ..\presets\frieze\*.json presets_frieze.js
-rem call make_preset ..\presets\klmn\*.json presets_klmn.js
+rem 
+call make_preset ..\presets\misc\*.json presets_misc.js
 rem 
 call make_preset ..\presets\wp\*.json presets_wp.js
 call make_preset ..\presets\nosym\*.json presets_nosym.js
